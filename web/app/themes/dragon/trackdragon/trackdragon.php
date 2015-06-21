397f834178cf0a9222aad361bd5b14a4e9517afe
Marty McGee <mcgee.marty@gmail.com>
2015-06-21 08:11:54 +0100

    added basic TrackDragon WordPress Plugin

---
+++ web/app/themes/dragon/trackdragon/trackdragon.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: TrackDragon
+ * Plugin URI: http://trackdragon.com
+ * Description: dragon.ninja WordPress Plugin
+ * Author: Marty McGee
+ * Author URI: http://companyjuice.com
+ * Version: 0.0.1
+ */
+?>
